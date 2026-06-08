9a65a1b07038bad1f31b36b1e2454b097da8b810
rishikeshThakar018 <rameshthakar410@gmail.com>
2026-06-08 16:46:29 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    // Write C code here
+    printf("Testing Demo.");
+    scanf("%d",&a);
+    printf("Your Enter This Number %d",a);
+    return 0;
+}
